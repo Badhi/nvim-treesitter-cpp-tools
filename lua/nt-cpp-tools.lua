8b8f635112646924b5b96dba695f9108156cdcb5
badhi <bmadhikarinayake@gmail.com>
2021-12-09 19:00:00 +0530

    renamed the template names with plugin names

--- lua/nt-cpp-tools.lua
+++ lua/nt-cpp-tools.lua
@@ -5,11 +5,11 @@ local M = {}
 -- TODO: In this function replace `module-template` with the actual name of your module.
 function M.init()
   require "nvim-treesitter".define_modules {
-    module_template = {
-      module_path = "module-template.internal",
+    nt_cpp_tools = {
+      module_path = "nt-cpp-tools",
       is_supported = function(lang)
         -- TODO: you don't want your queries to be named `awesome-query`, do you ?
-        return queries.get_query(lang, 'awesome-query') ~= nil
+        return queries.get_query(lang, 'queries') ~= nil
       end
     }
   }
---
+++ plugin/nt-cpp-tools.vim
@@ -0,0 +1,1 @@
+lua require("nt-cpp-tools").init()
