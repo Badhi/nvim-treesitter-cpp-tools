f8888267b848afe0eb17d6d75b70a875cf92a19d
badhi <bmadhikarinayake@gmail.com>
2022-01-13 09:44:23 +0530

    Fix:member variables identified as functions from the query

--- queries/cpp/outside_class_def.scm
+++ queries/cpp/outside_class_def.scm
@@ -1,6 +1,13 @@
 (class_specifier
     name: (type_identifier) @class_name
     body: (field_declaration_list
-        [(field_declaration) (declaration)]@member_function
+        [
+            (field_declaration
+                (function_declarator)
+            ) 
+            (declaration
+                (function_declarator)
+            )
+        ]@member_function
     )
 ) @class
