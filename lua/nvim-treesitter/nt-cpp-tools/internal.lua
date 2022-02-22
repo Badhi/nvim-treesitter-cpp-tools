b32cd680f510b28327b29378b6245f190a040aef
badhi <bmadhikarinayake@gmail.com>
2022-02-22 13:36:15 +0530

    Added support for unions

--- lua/nvim-treesitter/nt-cpp-tools/internal.lua
+++ lua/nvim-treesitter/nt-cpp-tools/internal.lua
@@ -201,7 +201,9 @@ local function find_class_details(member_node, member_data)
     local class_node = member_node:parent():type() == 'template_declaration' and
                         member_node:parent():parent():parent() or member_node:parent():parent()
     while class_node and
-        ( class_node:type() == 'class_specifier' or class_node:type() == 'struct_specifier') do
+        (class_node:type() == 'class_specifier' or
+        class_node:type() == 'struct_specifier' or
+        class_node:type() == 'union_specifier' ) do
         local class_data = {}
         class_data.name = t2s(ts_utils.get_node_text(class_node:field('name')[1]))
 
