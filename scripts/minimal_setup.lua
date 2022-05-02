e38cded0e789ae50442c1b203a5b4be36bd75942
badhi <bmadhikarinayake@gmail.com>
2022-05-02 10:43:11 +0530

    Fixed unit test issue due to change in the treesitter

--- scripts/minimal_setup.lua
+++ scripts/minimal_setup.lua
@@ -1,6 +1,6 @@
 --vim.cmd [[set runtimepath+=.]]
 vim.cmd [[runtime! plugin/plenary.vim]]
-vim.cmd [[runtime! plugin/nvim-treesitter.vim]]
+vim.cmd [[runtime! plugin/nvim-treesitter.lua]]
 vim.cmd [[runtime! plugin/nt-cpp-tools.vim]]
 
 vim.o.swapfile = false
