1ba5b926f4847f57804db6b9b3576a1b08d4a80f
Bhashitha Adhikarinayake <bmadhikarinayake@gmail.com>
2025-11-27 20:40:04 -0800

    Test setup update related to new treesitter

--- scripts/minimal_setup.lua
+++ scripts/minimal_setup.lua
@@ -1,6 +1,4 @@
 vim.cmd [[set runtimepath+=.]]
-vim.cmd [[set runtimepath+=/home/bhashith/lazy/nvim-treesitter/]]
-vim.cmd [[set runtimepath+=/home/bhashith/lazy/plenary.nvim/]]
 vim.cmd [[runtime! plugin/plenary.vim]]
 vim.cmd [[runtime! plugin/nvim-treesitter.lua]]
 vim.cmd [[runtime! plugin/nt-cpp-tools.vim]]
@@ -9,5 +7,6 @@ vim.o.swapfile = false
 vim.bo.swapfile = false
 vim.o.filetype = 'cpp'
 
-require("nvim-treesitter.configs").setup {
+require("nvim-treesitter").setup {
 }
+require'nvim-treesitter'.install { 'cpp'}
