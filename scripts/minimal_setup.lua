02bd85a8a676e12d2fe2696af4c14da70ae0e7a1
Bhashitha Adhikarinayake <bmadhikarinayake@gmail.com>
2025-11-27 20:57:19 -0800

    Updates the treesitter version

--- scripts/minimal_setup.lua
+++ scripts/minimal_setup.lua
@@ -1,4 +1,6 @@
 vim.cmd [[set runtimepath+=.]]
+vim.cmd [[set runtimepath+=./nvim-treesitter]]
+vim.cmd [[set runtimepath+=./plenary.nvim]]
 vim.cmd [[runtime! plugin/plenary.vim]]
 vim.cmd [[runtime! plugin/nvim-treesitter.lua]]
 vim.cmd [[runtime! plugin/nt-cpp-tools.vim]]
